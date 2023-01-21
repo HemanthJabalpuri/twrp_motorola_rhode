947a08c4bf8ae302d7473ed6cd3ea97c9ca3644e
HemanthJabalpuri <hemanthjabalpuri@gmail.com>
2023-01-21 16:02:51 +0530

    rhode: Use kernel and modules from S1SR32.38-124-3 * also use this patch for kernel module loading https://github.com/HemanthJabalpuri/android_bootable_recovery/commit/aaa920c5f723557fb7f0c282a6aea194be21ca54

--- device.mk
+++ device.mk
@@ -39,3 +39,6 @@ PRODUCT_SYSTEM_PROPERTY_BLACKLIST += \
 PRODUCT_PACKAGES += \
     qcom_decrypt \
     qcom_decrypt_fbe
+
+# Copy modules for depmod
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*.ko,$(DEVICE_PATH)/recovery/root/vendor/lib/modules/1.1,$(TARGET_COPY_OUT_RECOVERY)/root/vendor/lib/modules/1.1)
